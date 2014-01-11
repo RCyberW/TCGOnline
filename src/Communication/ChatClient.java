c0762f6213dacac945e797c279f35180c16a4095
Frank Chen <frank.chen@tabs.toshiba.com>
2014-01-10 23:56:10 -0800

    Updated with thread implementation

--- src/Communication/ChatClient.java
+++ src/Communication/ChatClient.java
@@ -1,6 +1,10 @@
 package Communication;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -10,8 +14,11 @@ import java.net.UnknownHostException;
  */
 public class ChatClient {
 	private Socket clientSocket;
-	
+	private int port;
+	private String serverName;
 	public ChatClient(String serverName, int port) {
+		this.port = port;
+		this.serverName = serverName;
 		try {
 			System.out.println("Connecting to " + serverName + " on port " + port);
 			clientSocket = new Socket(serverName, port);
@@ -21,6 +28,33 @@ public class ChatClient {
 
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		serverName = "Cyber";
+		// String serverName = "174.77.36.43";
+		try {
+			System.out.println("Connecting to " + serverName + " on port "
+					+ port);
+			
+			// establishing connection
+			Socket client = new Socket(serverName, port);
+			System.out.println(client.getPort());
+			System.out.println("Just connected to "
+					+ client.getRemoteSocketAddress());
+			
+			// sending request
+			OutputStream outToServer = client.getOutputStream();
+			DataOutputStream out = new DataOutputStream(outToServer);
+			out.writeUTF("Hello from " + client.getLocalSocketAddress());
+			
+			// receiving reply
+			InputStream inFromServer = client.getInputStream();
+			DataInputStream in = new DataInputStream(inFromServer);
+			System.out.println("Server says " + in.readUTF());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- src/Communication/Connector.java
+++ src/Communication/Connector.java
@@ -1,24 +1,64 @@
 package Communication;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 /**
- * @author Frank Chen
- * Connector receives instructions/chat messages from opposing player's ChatClient
+ * @author Frank Chen Connector receives instructions/chat messages from
+ *         opposing player's ChatClient
  */
 public class Connector {
 	private ServerSocket serverSocket;
-	
-	public Connector() {
+
+	public Connector(int port) {
+		InetAddress addr;
 		try {
-			InetAddress addr = InetAddress.getLocalHost();
-			SocketAddress endpoint = new InetSocketAddress(addr, 5000);
+			addr = InetAddress.getLocalHost();
+			SocketAddress endpoint = new InetSocketAddress(addr, port);
 			serverSocket = new ServerSocket();
 			serverSocket.bind(endpoint);
-		} catch (Exception e) {
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		try {
+			System.out.println("Waiting for client on port "
+					+ serverSocket.getLocalSocketAddress().toString() + "...");
+			// establishing connection
+			Socket server = serverSocket.accept();
+			System.out.println("Just connected to "
+					+ server.getRemoteSocketAddress());
+
+			// receiving request
+			DataInputStream in = new DataInputStream(server.getInputStream());
+			System.out.println(in.readUTF());
+
+			String reply = "";
+
+			// sending reply
+			DataOutputStream out = new DataOutputStream(
+					server.getOutputStream());
+			out.writeUTF("Thank you for connecting to "
+					+ server.getLocalSocketAddress() + "\n" + reply);
+
+		} catch (SocketTimeoutException s) {
+			System.out.println("Socket timed out!");
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
